fb120289f25f20122528bb01b0f4af6866e82963
Dhruv110804 <dhruv.k@kalvium.community>
2024-01-25 16:02:22 +0530

    File is connected with DB

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const URI = 'mongodb+srv://dhruvk:q6GkQB4Kg3jvSHFd@dhruv-bike-info.dga5pzc.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(URI)
+.then(()=>{
+    app.get('/',(req,res)=>{
+        res.json({Connection_Status:'Connected'})
+    })
+}).catch((err)=>{
+    app.get('/',(req,res)=>{
+        res.json({Connection_Status:'Not Connected'})
+    })
+})
+
+
 
 app.get('/ping',(req,res)=>{
     res.json({message:'Pong'})
