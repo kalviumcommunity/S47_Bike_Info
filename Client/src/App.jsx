1348346fd0ef78b58bb772a5bf9bdd156116b4df
Dhruv110804 <dhruv.k@kalvium.community>
2024-02-01 14:54:39 +0530

    Entity added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
-
+import Bikeinfo from './components/Bikeinfo'
 function App() {
-  
+    const Data =  {   
+      "id":0,
+      "company" : "Royal Enfiled",
+      "model" : "Classic 350",
+      "price" : "Rs 1.93 - 2.24 lakh",
+      "mileage" : "37.77 Kmpl",
+      "engine" : "349 CC",
+      "fuel_capacity" : "13 litre",
+      "weight" : "195 Kg",
+      "img":"https://english.cdn.zeenews.com/sites/default/files/styles/zm_700x400/public/2021/12/20/997676-royal-enfield-classic-350.jpg?im=Resize=(400,229)"
+    }
   return (
     <>
-     <h1>Welcome in the world of bikes.</h1>
+      <div className='nav'>
+        <h1 className='title'>BIKE-INFO</h1>
+      </div>
+      <Bikeinfo Data = {Data}/>
     </>
   )
 }
