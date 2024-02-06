5fa828f96804474e7ce41efe3bcc347762f1947f
Dhruv110804 <dhruv.k@kalvium.community>
2024-02-06 22:46:12 +0530

    Update and Delete functionality added

--- Client/src/components/Bikeinfo.jsx
+++ Client/src/components/Bikeinfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect,useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 const Bikeinfo = () => {
     // const Data = props.Data
     const [Data,setData] = useState([])
@@ -8,15 +9,28 @@ const Bikeinfo = () => {
     useEffect(()=>{
       fetch('http://localhost:4000/DataBase')
       .then(data=>data.json())
-      .then(res =>setData(res))
-    })
+      .then(res =>{
+        setData(res)
+        // console.log(res)
+      })
+    },[])
+
+    const HandelDelete=(id)=>{
+      // console.log("Delete button clicked",id);
+      axios.delete(`http://localhost:4000/DeleteData/${id}`)
+      .then(res=>{
+        console.log(res)
+        window.location.reload(true)
+      }).catch(err=>console.log(err))
+    }
+
   return (
     <div className='body'>
       <div className='nav'>
         <h1 className='title'>BIKE-INFO</h1>
       </div>
       <h1 className='insert'>For Insert more Data <Link to='/InsertData' state={{color:'blue'}}>Click Here!</Link></h1>
-      {Data.map((Data,index)=>{
+      {Data && Data.map((Data,index)=>{
         return(
         <div key={index} className='box'>
           <img src={Data.img} alt="image" className='image'/>
@@ -25,7 +39,10 @@ const Bikeinfo = () => {
             <h3>Model: {Data.model}</h3>
             <p>Price: {Data.price}</p>
             <p>Mileage: {Data.mileage}</p>
-           
+          </div>
+          <div className='btn'>
+            <Link to={`/UpdateData/${Data._id}`}><button>Update</button></Link>
+            <button onClick={()=>HandelDelete(Data._id)}>Delete</button>
           </div>
         </div>
         )
--- server/server.js
+++ server/server.js
@@ -14,13 +14,41 @@ const URI = 'mongodb+srv://dhruvk:q6GkQB4Kg3jvSHFd@dhruv-bike-info.dga5pzc.mongo
 mongoose.connect(URI)
 
 app.get('/DataBase',(req,res)=>{
-    DataModel.find()
+    DataModel.find({})
     .then(data=>res.json(data))
     .catch((err)=>res.json(err))
 })
 
 app.post('/InsertData',(req,res)=>{
     DataModel.create(req.body)
+    .then(data=>res.json(data))
+    .catch((err)=>res.json(err))
+})
+
+app.get('/GetData/:id',(req,res)=>{
+    const {id} = req.params
+    DataModel.findById(id)
+    .then(data=>res.json(data))
+    .catch((err)=>res.json(err))
+})
+
+app.put('/UpdateData/:id',(req,res)=>{
+    const {id} = req.params
+    const {company,model,price,mileage,img} = req.body
+    DataModel.findByIdAndUpdate(id,{
+        company,
+        model,
+        price,
+        mileage,
+        img
+    },{new:true})
+    .then(data=>res.json(data))
+    .catch((err)=>res.json(err))
+})
+
+app.delete('/DeleteData/:id',(req,res)=>{
+    const {id} = req.params
+    DataModel.findByIdAndDelete({_id:id})
     .then(data=>res.json(data))
     .catch((err)=>res.json(err))
 })
